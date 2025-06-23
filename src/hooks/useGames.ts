dde5093d547767ff0f3545fbc9821c13de8680cd
aerializer <aerializer@gmail.com>
2025-06-23 16:39:24 +0800

    Implement infinite queries

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
 import { CACHE_KEY_GAMES } from "../constant";
 import APIClient, { FetchResponse } from "../services/api-client";
@@ -29,18 +29,22 @@ const useGames = (gameQuery: GameQuery) => useData<Game>(
 
 const apiClient = new APIClient<Game>("/games");
 
-const useGames = (gameQuery: GameQuery) => useQuery<FetchResponse<Game>, Error>({
+const useGames = (gameQuery: GameQuery) => useInfiniteQuery<FetchResponse<Game>, Error>({
   queryKey: [CACHE_KEY_GAMES, gameQuery],
-  queryFn: () => 
+  queryFn: ({pageParam = 1 }) => 
     apiClient
       .getAll({
         params: {
           genres: gameQuery.genre?.id, 
           parent_platforms: gameQuery.platform?.id, 
           ordering: gameQuery.sortOrder,
-          search: gameQuery.searchText
+          search: gameQuery.searchText,
+          page: pageParam
         },
       }),
-})
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.next ? allPages.length + 1 : undefined;
+    }
+});
 
 export default useGames;
